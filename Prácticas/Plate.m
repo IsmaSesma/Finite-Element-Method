4ca6946b0dec22f535ff57a29553dd52b3cb59b0
IsmaSesma <ismasesma@outlook.com>
2022-05-05 13:54:42 +0200

    5/5 uni

--- Prácticas/Plate.m
+++ Prácticas/Plate.m
@@ -2,18 +2,19 @@
 %           FEM MODEL OF A PLATE USING RECTANGULAR ELEMENTS 
 % ***********************************************************************
 
-clc;clear;close all;
+%clc;clear;close all;
 
 %% MASIC AND GEOMETRIC DATA
 
-plate.E = 2E9; plate.nu = 0.3;
-plate.a = 0.2; plate.b = 0.2;                % Plate's dimensions
-plate.t = 0.04;                              % Plate's thickness
+plate.E = 10920; plate.nu = 0.3;
+plate.a = 1; plate.b = 1;                % Plate's dimensions
+plate.t = 0.1;                              % Plate's thickness
 plate.m = 0.5;                               % Plate's mass
 
 %% INPUT DATA
 
-ne_s = 3;                     % Number of bidimensional elements per side of the plate
+ne_s = 20;                     % Number of bidimensional elements per side of the plate
+nne = 4;                      % Number of nodes per element
 nn_s = ne_s+1;                % Number of nodes per side
 ne = ne_s^2;                  % Number of bidimensional elements in the whole plate
 nn = nn_s^2;                  % Number of nodes
@@ -35,17 +36,15 @@ coord = zeros(nn_s,2);                        % Coordinates of the external node
 coord(:,1) = 0:plate.a/(nn_s-1):plate.a;      % X coordinates
 coord(:,2) = 0:plate.b/(nn_s-1):plate.b;      % Y coordinates
 
-[X,Y] = meshgrid(coord(:,1),coord(:,2));    % Mesh to represent the plate
+[X,Y] = meshgrid(coord(:,1),coord(:,2));      % Mesh to represent the plate
 Z = zeros(nn_s);
 surf(X,Y,Z)
 
-K = zeros(nn*dofn);
-D = plate.E/(1 - plate.nu)^2*[1 plate.nu 0; plate.nu 1 0; 0 0 (1 - plate.nu)/2];
-xe = plate.a; ye = plate.b;
-Je = plate.a*plate.b/4;
+coord_n = zeros(nn_s,2*nn_s);
+coord_n(:,1:2:end) = X;
+coord_n(:,2:2:end) = Y;
 
-Ke = zeros(dofn*4);
-index = zeros(2);
+coord_n = reshape(coord_n,[nn_s,2,nn_s]);       % Y coordinates are in the first column and X coordinates in the second
 
 % Vector with the relative position of every element of the beam
 row_col = zeros(ne,2);
@@ -63,16 +62,30 @@ for e = 1:ne
     node4 = (ne_s + 1)*row_col(e,1) + row_col(e,2) + 1;
 
     connectivity(:,:,e) = [node1 node2; node3 node4];
-
-    
 end
+
+% Stiffness Matrix
+K = zeros(DOF);
+D = I*plate.E/(1 - plate.nu^2)*[1 plate.nu 0; plate.nu 1 0; 0 0 (1 - plate.nu)/2];
+index = zeros(2);
 
 for e = 1:ne
    index = connectivity(:,:,e);
-   x1 = X(index(1,1),1); x2 = X(index(1,2),1);
+   x2 = coord_n(mod((e-1),ne_s) + 2,2,floor((e-1)/ne_s) + 1); x1 = coord_n(mod((e-1),ne_s) + 1,2,floor((e-1)/ne_s) + 1);
+   y2 = coord_n(mod((e-1),ne_s) + 1,1,floor((e-1)/ne_s) + 2); y1 = coord_n(mod((e-1),ne_s) + 1,1,floor((e-1)/ne_s) + 1);
+   xe = x2 - x1; ye = y2 - y1;                                                  % Lengths of the element on both directions
+   Je = xe*ye/4;                                                                % Transformation's Jacobian
+   dofe = [index(1,1)*dofn - 2 index(1,1)*dofn - 1 index(1,1)*dofn...           % DOF of the element
+           index(1,2)*dofn - 2 index(1,2)*dofn - 1 index(1,2)*dofn...
+           index(2,1)*dofn - 2 index(2,1)*dofn - 1 index(2,1)*dofn...
+           index(2,2)*dofn - 2 index(2,2)*dofn - 1 index(2,2)*dofn]; 
+
+Ke = zeros(dofn*nne);                      % Initialization of element stiffness matrix
+
     for i = 1:2
+        chi = chi_ip.s(i);
         for j = 1:2
-            chi = chi_ip.s(i); eta = chi_ip.s(j);                   % Value of interpolation points
+            eta = chi_ip.s(j);                                      % Value of interpolation points
             w = w_ip.s(i);                                          % Weight of interpolation
             dNdchi_1 = -(1 - eta)/4; dNdeta_1 = -(1 - chi)/4;       % Derivates of form functions
             dNdchi_2 = (1 - eta)/4; dNdeta_2 = -(1 + chi)/4; 
@@ -86,6 +99,9 @@ for e = 1:ne
             Ke = Ke + B'*D*B*Je*w;
         end
     end
+
+    K(dofe,dofe) = K(dofe,dofe) + Ke;
+
 end
 
 %% FUNCTIONS
